f9df72d5763037a9e73588d3847474b7a28e9dc7
andrew-youngjae <andrewlyj1@gmail.com>
2024-11-04 16:14:06 +0900

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by YoungJae Lee on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoTests/ToDoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoTests.swift
+//  ToDoTests
+//
+//  Created by YoungJae Lee on 11/4/24.
+//
+
+import Testing
+@testable import ToDo
+
+struct ToDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
